2cb137cf2f407892e32bfae22657f3d9b28f934e
Rodrigo Marques da Silva <re036971@resource.com.br>
2019-07-23 10:14:33 -0300

    step 6 - messagetoast

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/messagetoast"
+ ], function (Controller, messagetoast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
         onShowHello : function () {
            // show a native JavaScript alert
-           alert("Hello World");
+           messagetoast.show("Teste message toast")
         }
      });
  });
