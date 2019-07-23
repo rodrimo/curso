72f1a015a805354aa0cd5ce2939ad3fc20b916d8
Rodrigo Marques da Silva <re036971@resource.com.br>
2019-07-23 10:22:28 -0300

    step 7 - Json

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,12 +1,25 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/messagetoast"
- ], function (Controller, messagetoast) {
+    "sap/m/messagetoast",
+    "sap/ui/model/json/JSONModel"
+ ], function (Controller, MessageToast, JSONModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+        onInit : function () {
+            // set data model on view
+            var oData = {
+               recipient : {
+                  name : "World"
+               }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+         },
+
         onShowHello : function () {
            // show a native JavaScript alert
-           messagetoast.show("Teste message toast")
+           MessageToast.show("Teste message toast")
         }
      });
  });
